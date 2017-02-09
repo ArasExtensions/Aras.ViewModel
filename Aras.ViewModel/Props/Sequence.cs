139b99513c4d27768c59e3caaa22d93e8f5d255a
Richard Mason <richard.mason@processwall.com>
2017-02-09 14:46:00 +0000

    Added Sequence Property

---
+++ Aras.ViewModel/Props/Sequence.cs
@@ -0,0 +1,103 @@
+﻿/*  
+  Aras.Model provides a .NET cient library for Aras Innovator
+
+  Copyright (C) 2015 Processwall Limited.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see http://opensource.org/licenses/AGPL-3.0.
+ 
+  Company: Processwall Limited
+  Address: The Winnowing House, Mill Lane, Askham Richard, York, YO23 3NW, United Kingdom
+  Tel:     +44 113 815 3440
+  Email:   support@processwall.com
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aras.ViewModel.Properties
+{
+    [Attributes.ClientControl("Aras.View.Properties.Sequence")]
+    public class Sequence : Property
+    {
+
+        private System.String _value;
+        [Attributes.Property("Value", Attributes.PropertyTypes.String, true)]
+        public System.String Value
+        {
+            get
+            {
+                return this._value;
+            }
+            set
+            {
+                if (this._value != value)
+                {
+                    this._value = value;
+                    this.OnPropertyChanged("Value");
+                }
+            }
+        }
+
+        protected override void CheckBinding(object Binding)
+        {
+            base.CheckBinding(Binding);
+
+            if (!(Binding is Model.Properties.Sequence))
+            {
+                throw new Model.Exceptions.ArgumentException("Binding must be of type Aras.Model.Properties.Sequence");
+            }
+        }
+
+        protected override void AfterBindingChanged()
+        {
+            base.AfterBindingChanged();
+
+            if (this.Binding != null)
+            {
+                this.Value = (System.String)((Model.Properties.Sequence)this.Binding).Value;
+            }
+        }
+
+        protected override void BeforeBindingChanged()
+        {
+            base.BeforeBindingChanged();
+        }
+
+        protected override void Property_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.Property_PropertyChanged(sender, e);
+
+            if (this.Binding != null)
+            {
+                switch (e.PropertyName)
+                {
+                    case "Value":
+                        this.Value = (System.String)((Model.Properties.Sequence)this.Binding).Value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public Sequence(Manager.Session Session)
+            : base(Session)
+        {
+      
+        }
+    }
+}
